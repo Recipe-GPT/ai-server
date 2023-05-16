dc7cdaf7040a181245d744ddc946fddb62d8233d
leehj050211 <leehj050211@gmail.com>
2023-05-16 15:53:33 +0900

    ADD: 환경 변수 예시 파일

---
+++ src/.prompt.env.example.ts
@@ -0,0 +1,21 @@
+const OPENAI_MODEL_DEFAULT_PROMPT = [
+{
+"role": "system",
+"content": ""
+},
+{
+"role": "assistant",
+"content": ""
+},
+{
+"role": "user",
+"content": ""
+}
+];
+
+const FOOD_REGEX = /\//;
+
+export {
+  OPENAI_MODEL_DEFAULT_PROMPT,
+  FOOD_REGEX
+}

40e4dbbcb352898994e06dbc3bcfe39b96b2efd5
leehj050211 <leehj050211@gmail.com>
2023-05-27 23:21:07 +0900

    ADD: OpenAI API를 통한 레시피 생성 기능

--- src/domain/recipe/controller/recipeController.ts
+++ src/domain/recipe/controller/recipeController.ts
@@ -35,23 +35,46 @@ router.post('/recipe/mock', (req, res) => {
   );
 });
 
-router.post('/recipe/api', (req, res) => {
+router.post('/recipe/api', async (req, res) => {
   /*  #swagger.tags = ['Recipe']
       #swagger.summary = 'API를 통해 레시피 생성'
       #swagger.description = 'OpenAI의 GPT API를 통한 레시피 생성 API' */
   /*  #swagger.security = [{
       "apiKeyAuth": []
   }] */
+  /* #swagger.requestBody = {
+      description: '요리 이름, 설명, 식재료, 양념 정보',
+      required: true,
+      schema: { $ref: "#/definitions/RecipeReq" }
+  } */
+  /* #swagger.responses[200] = {
+      schema: { "$ref": "#/definitions/RecipeRes" },
+      description: "성공" } */
+  /* #swagger.responses[400] = {
+      schema: { "$ref": "#/definitions/RecipeBadRequestRes" },
+      description: "요청이 양식에 맞지않음" } */
   /* #swagger.responses[401] = {
       schema: { "$ref": "#/definitions/UnAuthorizedRes" },
       description: "인증 안됨" } */
+  /* #swagger.responses[409] = {
+      schema: { "$ref": "#/definitions/RecipeConflictRes" },
+      description: "사용자의 요청을 토대로 AI가 레시피를 생성하는데 문제가 발생함" } */
   /* #swagger.responses[500] = {
       schema: { "$ref": "#/definitions/InternalServerErrorRes" },
       description: "내부 서버 문제" } */
-  /* #swagger.responses[501] = {
-      schema: { "$ref": "#/definitions/NotImplementedRes" },
-      description: "아직 구현안됨" } */
-  throw new NotImplementedException();
+  /* #swagger.responses[502] = {
+    schema: { "$ref": "#/definitions/OpenAiBadGatewayRes" },
+    description: "OpenAi 서버 문제" } */
+  
+  const payload: RecipeReq = {
+    name: req.body.name,
+    description: req.body.description,
+    ingredients: req.body.ingredients,
+    seasonings: req.body.seasonings
+  };
+  res.json(
+    await RecipeService.generateRecipeByOpenAiApi(payload)
+  );
 });
 
 router.post('/recipe/proxy', async (req, res) => {
@@ -81,6 +104,9 @@ router.post('/recipe/proxy', async (req, res) => {
   /* #swagger.responses[500] = {
       schema: { "$ref": "#/definitions/InternalServerErrorRes" },
       description: "내부 서버 문제" } */
+  /* #swagger.responses[502] = {
+    schema: { "$ref": "#/definitions/OpenAiBadGatewayRes" },
+    description: "OpenAi 프록시 서버 문제" } */
   
   const payload: RecipeReq = {
     name: req.body.name,
--- src/domain/recipe/service/recipeService.ts
+++ src/domain/recipe/service/recipeService.ts
@@ -4,9 +4,34 @@ import { RecipePromptService } from "@/domain/recipe/service/recipePromptService
 import { RecipeReq } from "@/domain/recipe/type/recipeReq";
 import { RecipeRes } from "@/domain/recipe/type/recipeRes";
 import InternalServerException from "@/global/error/exceptions/internalServerException";
+import { OpenAiApiService } from "@/infrastructure/openai/openAiApiService";
 import { OpenAiProxyService } from "@/infrastructure/openai/openAiProxyService";
 import { PromptService } from "@/infrastructure/prompt/promptService";
 import { Prompt } from "@/infrastructure/prompt/type/prompt";
+
+const generateRecipeByMockData = () => {
+  const text = RECIPE_MOCK_RESPONSE;
+  return RecipePromptService.parseAiResponse(text);
+}
+
+const generateRecipeByOpenAiApi = async (req: RecipeReq): Promise<RecipeRes> => {
+  await PolicyService.contentPolicyCheck(
+    PromptService.encodeName(req.name),
+    PromptService.encodeDescription(req.description),
+    PromptService.encodeIngredients(req.ingredients),
+    PromptService.encodeSeasonings(req.seasonings)
+  );
+  
+  if (!RECIPE_PROMPT) {
+    throw new InternalServerException('서버에 문제가 발생하였습니다. 프롬프트 설정을 확인해주세요.');
+  }
+  const prompts: Prompt[] = RECIPE_PROMPT.concat(
+    RecipePromptService.getUserPrompt(req)
+  );
+  
+  const text = await OpenAiApiService.generate(prompts);
+  return RecipePromptService.parseAiResponse(text);
+};
 
 const generateRecipeByProxy = async (req: RecipeReq): Promise<RecipeRes> => {
   await PolicyService.contentPolicyCheck(
@@ -27,12 +52,8 @@ const generateRecipeByProxy = async (req: RecipeReq): Promise<RecipeRes> => {
   return RecipePromptService.parseAiResponse(text);
 };
 
-const generateRecipeByMockData = () => {
-  const text = RECIPE_MOCK_RESPONSE;
-  return RecipePromptService.parseAiResponse(text);
-}
-
 export const RecipeService = {
-  generateRecipeByProxy,
-  generateRecipeByMockData
+  generateRecipeByMockData,
+  generateRecipeByOpenAiApi,
+  generateRecipeByProxy
 };
